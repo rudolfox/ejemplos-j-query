a3f7081ab8280f0b53e0a4ec79743d7290f38d2e
rudolfox <rudolf090594@gmail.com>
2017-03-08 14:01:12 -0400

    Eventos jquery

---
+++ holajquery.js
@@ -0,0 +1,14 @@
+/*$(document).ready(saluda)
+
+function saluda(){
+  alert($('h1').text())
+}*/
+
+$(function(){
+
+      //alert($('h1').text())
+      //Se crea un objeto nuevo del tipo parrafo
+      $nuevo=$('<p>Parrafo nuevo<p>')
+      $('h2').after($nuevo)
+
+})
