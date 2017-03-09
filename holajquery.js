5cab502667befa08f335ca53b3fe52edccdb3123
rudolfox <rudolf090594@gmail.com>
2017-03-09 14:01:57 -0400

    Ejemplos de eventos

--- holajquery.js
+++ holajquery.js
@@ -4,11 +4,13 @@ function saluda(){
   alert($('h1').text())
 }*/
 
-$(function(){
+/*$(function(){
 
       //alert($('h1').text())
       //Se crea un objeto nuevo del tipo parrafo
       $nuevo=$('<p>Parrafo nuevo<p>')
       $('h2').after($nuevo)
+      $(selector).after(content)
+  
+})*/
 
-})
